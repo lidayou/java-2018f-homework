46dc43c90dac6e9970999b7b89a861aaf374736a
Qcer17 <qin_tianyu@hotmail.com>
2018-09-12 14:56:51 +0800

    秦天-161220100

--- 20180911/秦天-161220100/MySort.java
+++ 20180911/秦天-161220100/MySort.java
@@ -1,30 +1,25 @@
-import java.util.Random;
 public class MySort {
 	static void swap(int nums[],int id1,int id2) {
 		int temp=nums[id1];
 		nums[id1]=nums[id2];
 		nums[id2]=temp;
 	}
-	static void quicksort(int nums[],int left,int right) {
+	static void quickSort(int nums[],int left,int right) {
 		if(left>=right-1)return;
 		int pivot=nums[left],pos=left;
 		for(int i=left+1;i<right;i++) {
 			if(nums[i]<pivot) {
-				swap(nums, pos, i);
 				pos++;
+				swap(nums, pos, i);
 			}
 		}
-		nums[pos]=pivot;
-		quicksort(nums, pos+1, right);
-		quicksort(nums, left, pos);
+		swap(nums,pos,left);
+		quickSort(nums, pos+1, right);
+		quickSort(nums, left, pos);
 	}
 	public static void main(String args[]) {
-		int a[]=new int[20];
-		Random random=new Random();
-		for(int i=0;i<a.length;i++) {
-			a[i]=random.nextInt(100);
-		}
-		quicksort(a,0,a.length);
-		for(int x:a)System.out.println(x);
+		int nums[]=new int[]{5,1,2,7,4,6,10,8,9,11,13,12,3};
+		quickSort(nums,0,nums.length);
+		for(int x:nums)System.out.print(x+" ");
 	}
 }
