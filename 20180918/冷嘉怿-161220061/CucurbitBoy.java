f31423fc4ab487cf3c777f14181c9da1b2ceaf95
lengjiayi <824543859@qq.com>
2018-09-18 17:40:19 +0800

    CBs are ready

--- 20180918/冷嘉怿-161220061/CucurbitBoy.java
+++ 20180918/冷嘉怿-161220061/CucurbitBoy.java
@@ -6,7 +6,16 @@ enum CucurbitBoys{
 public class CucurbitBoy {
     static protected String[] names={"老大","老二","老三","老四","老五","老六","老七"};
     static protected String[] colors={"红色","橙色","黄色","绿色","青色","蓝色","紫色"};
+    static protected int nextid=0;
     private CucurbitBoys id;
+    CucurbitBoy() throws Exception {
+        if(CucurbitBoy.nextid>=7)
+        {
+            throw new Exception("葫芦娃只有七个！");
+        }
+        this.id=CucurbitBoys.values()[CucurbitBoy.nextid];
+        CucurbitBoy.nextid++;
+    }
     public void tellColor()
     {
         System.out.print(CucurbitBoy.colors[this.id.ordinal()]);
