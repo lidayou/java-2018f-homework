704afa01a9f07fc251db78fc5c59618807493945
Young <895254752@qq.com>
2018-09-20 19:18:43 +0800

    161220096 for submit

--- 20180918/欧阳鸿荣-161220096/CalabashBoys.java
+++ 20180918/欧阳鸿荣-161220096/CalabashBoys.java
@@ -5,11 +5,12 @@ package calabashBrothers;
  * @ Description：葫芦娃
  */
 
-enum  Color{
-    RED("红色"),ORANGE("橙色"),YELLOW("黄色"),GREEN("绿色"),VERDANT("青色"),BLUE("蓝色"),PURPLE("紫色"); // 红橙黄绿蓝靛紫
+enum Color {
+    RED("红色"), ORANGE("橙色"), YELLOW("黄色"), GREEN("绿色"), VERDANT("青色"), BLUE("蓝色"), PURPLE("紫色"); // 红橙黄绿蓝靛紫
 
     private String colorName;
     private int colorNumber;
+
     Color(String colorName) {
         this.colorName = colorName;
         this.colorNumber = 7;
@@ -20,23 +21,25 @@ enum  Color{
     }
 }
 
-enum CalabashBrothers{
+enum CalabashBrothers {
 
-    RED_BOY("老大",Color.RED),
-    ORANGE_BOY("老二",Color.ORANGE),
-    YELLOW_BOY("老三",Color.YELLOW),
-    GREEN_BOY("老四",Color.GREEN),
-    VERDANT_BOY("老五",Color.VERDANT),
-    BLUE_BOY("老六",Color.BLUE),
-    PURPLE_BOY("老七",Color.PURPLE);
+    RED_BOY("老大", Color.RED, 0),
+    ORANGE_BOY("老二", Color.ORANGE, 1),
+    YELLOW_BOY("老三", Color.YELLOW, 2),
+    GREEN_BOY("老四", Color.GREEN, 3),
+    VERDANT_BOY("老五", Color.VERDANT, 4),
+    BLUE_BOY("老六", Color.BLUE, 5),
+    PURPLE_BOY("老七", Color.PURPLE, 6);
 
     private String name;
     private Color color;
+    private int rank;
     private int number;
 
-    CalabashBrothers(String name, Color color) {
+    CalabashBrothers(String name, Color color, int rank) {
         this.name = name;
         this.color = color;
+        this.rank = rank;
         this.number = 7;
     }
 
@@ -48,8 +51,8 @@ enum CalabashBrothers{
         return color.getColorName();
     }
 
-    public int getRank(){
-        return this.ordinal();
+    public int getRank() {
+        return this.rank;
     }
 
 
