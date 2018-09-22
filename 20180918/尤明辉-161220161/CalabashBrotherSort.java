1140fa7202ca114d9808481e8a85bd1a0d35f7cf
YmHAHAHa <1158041380@qq.com>
2018-09-22 16:36:38 +0800

    9/22

--- 20180918/尤明辉-161220161/CalabashBrotherSort.java
+++ 20180918/尤明辉-161220161/CalabashBrotherSort.java
@@ -2,37 +2,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Collections;
 
-enum CalabashBrother {
-    ONE(1, "老大", "红色"),
-    TWO(2, "老二", "橙色"),
-    THREE(3, "老三", "黄色"),
-    FOUR(4, "老四", "绿色"),
-    FIVE(5, "老五", "青色"),
-    SIX(6, "老六", "蓝色"),
-    SEVEN(7, "老七", "紫色");
-
-    public final int order;
-    private final String orderChn;
-    private final String color;
-
-    CalabashBrother(int ranking, String rankingChn, String colour) {
-        this.order = ranking;
-        this.orderChn = rankingChn;
-        this.color = colour;
-    }
-
-    public void reportOrder() {
-        System.out.print(this.orderChn + " ");
-    }
-
-    public void reportColor() {
-        System.out.print(this.color + " ");
-    }
-
-    public void reportChange(int pos1, int pos2) {
-        System.out.println(orderChn + "：" + pos1 + " -> " + pos2);
-    }
-}
 
 public class CalabashBrotherSort {
     //private final CalabashBrother[] brothers;
@@ -76,7 +45,7 @@ public class CalabashBrotherSort {
 
     private void binarySort() {
         for (int i = 1; i < brothersList.size(); ++i) {
-            int low = 0, high = i - 1, mid = 0;
+            int low = 0, high = i - 1, mid;
             CalabashBrother tmp = brothersList.get(i);
             while (low <= high) {
                 mid = (low + high) >> 1;
