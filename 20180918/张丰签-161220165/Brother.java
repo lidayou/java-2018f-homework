82aec80012e9f416c1b566afb7bca5c960398c60
zfq005 <542675575@qq.com>
2018-09-25 19:27:37 +0800

    改成枚举

--- 20180918/张丰签-161220165/Brother.java
+++ 20180918/张丰签-161220165/Brother.java
@@ -1,10 +1,13 @@
+enum Color{
+    RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE
+}
 public class Brother  {
     private String name;//葫芦娃名字
-    private String color;//葫芦娃颜色
+    private Color color;//葫芦娃颜色
     int position;//葫芦娃当前位置
     int number;//葫芦娃应当的排序
 
-    Brother(int get_number,String get_name,String get_color){
+    Brother(int get_number,String get_name,Color get_color){
         number=get_number;
         name=get_name;
         color=get_color;
@@ -71,10 +74,19 @@ class test{
     }
     public static void main(String[] args){
         String[] name={"老大","老二","老三","老四","老五","老六","老七"};//初始化对象
-        String[] color={"赤","橙","黄","绿","青","蓝","紫"};
-        Brother[] brother=new Brother[7];
-        for(int i=0;i<7;i++) {
-            brother[i] = new Brother(i + 1, name[i], color[i]);
+        Brother[] brother=new Brother[brother_num];
+        for(int i=0;i<brother_num;i++){
+            switch(i){
+                case 0:brother[i]=new Brother(i,name[i],Color.RED);break;
+                case 1:brother[i]=new Brother(i,name[i],Color.ORANGE);break;
+                case 2:brother[i]=new Brother(i,name[i],Color.YELLOW);break;
+                case 3:brother[i]=new Brother(i,name[i],Color.GREEN);break;
+                case 4:brother[i]=new Brother(i,name[i],Color.CYAN);break;
+                case 5:brother[i]=new Brother(i,name[i],Color.BLUE);break;
+                case 6:brother[i]=new Brother(i,name[i],Color.PURPLE);break;
+                default:
+                    break;
+            }
         }
         int[] get_pos1={2,1,5,4,7,3,6};//初始排序，大娃第二个，二娃第一个，以此类推
         int[] get_pos2={4,7,2,3,1,6,5};
@@ -84,16 +96,16 @@ class test{
         }
         change_pos(brother);
         BubbleSort(brother);
-        for(int i=0;i<7;i++){
+        for(int i=0;i<brother_num;i++){
             brother[i].report_name();
         }
         System.out.println("二分排序：");
-        for(int i=0;i<7;i++){
+        for(int i=0;i<brother_num;i++){
             brother[i].position=get_pos2[i];
         }
         change_pos(brother);
         BinarySort(brother);
-        for(int i=0;i<7;i++){
+        for(int i=0;i<brother_num;i++){
             brother[i].report_color();
         }
     }
