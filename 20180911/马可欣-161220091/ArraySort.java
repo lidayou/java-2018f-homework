8630b3f928ea9b174de3991a602866d87cf08d4d
mkx22 <3476799672@qq.com>
2018-10-06 13:28:55 +0800

    homework

--- 20180911/马可欣-161220091/ArraySort.java
+++ 20180911/马可欣-161220091/ArraySort.java
@@ -1,8 +1,20 @@
-import java.util.Arrays;
-
 public class ArraySort {
     public static void main(String[] args) {
-        int []a={1,2,3,6,5,7,8};
-        Arrays.sort(a);
+        int[] a = {1, 2, 4, 3, 5, 6, 0};
+        int i = 0, j = 0;
+        int min = a[0];
+        for (i = 0; i < a.length; i++) {
+            min=a[i];
+            for (j = i; j < a.length; j++) {
+                if (a[j] < min) {
+                    int temp=a[j];
+                    a[j]=min;
+                    a[i]=temp;
+                    min=temp;
+                }
+            }
+        }
+        for(i=0;i<a.length;i++)
+            System.out.print(a[i]+" ");
     }
 }
