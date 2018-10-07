3afda22a9128555fab7f9bd8f19fc1107082332d
ztw <TingweiZhu33@gmail.com>
2018-10-07 15:38:15 +0800

    homework3

---
+++ 20180925/src/creature/Grandpa.java
@@ -0,0 +1,15 @@
+package creature;
+
+public class Grandpa extends Good implements Cheerable {
+
+	@Override
+	public void cheer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String toString() {
+		return "爷爷";
+	}
+}
