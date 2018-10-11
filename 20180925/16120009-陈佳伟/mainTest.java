cc4e5c22a896552bd5c86551359fa455c8595bff
Notteaquail <1370597609@qq.com>
2018-10-11 17:00:21 +0800

    deleted:    "20180925/16120009-\351\231\210\344\275\263\344\274\237/Battlefield.java" 	deleted:    "20180925/16120009-\351\231\210\344\275\263\344\274\237/CalabashBoy.java" 	deleted:    "20180925/16120009-\351\231\210\344\275\263\344\274\237/FormationMaker.java" 	deleted:    "20180925/16120009-\351\231\210\344\275\263\344\274\237/mainTest.java" 	deleted:    "20180925/16120009-\351\231\210\344\275\263\344\274\237/theCalabashGodHand.java"  Untracked files: 	20180925/

--- 20180925/16120009-陈佳伟/mainTest.java
+++
@@ -1,8 +0,0 @@
-package Homework.CalabashBrothers_2;
-
-public class mainTest {
-    public static void main(String args[]){
-        theCalabashGodHand a = new theCalabashGodHand();
-        a.BattlefieldLayout();
-    }
-}
