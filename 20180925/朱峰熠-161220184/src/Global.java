b2a5be0d2b5c6768017f72b1c0a0f9649b0fcfbb
Chasenzfy <934542721@qq.com>
2018-10-14 21:04:04 +0800

    作业三

---
+++ 20180925/朱峰熠-161220184/src/Global.java
@@ -0,0 +1,8 @@
+public class Global {
+    public static final int EMPTY = 0;
+    public static final int MONSTER = 1;
+    public static final int CALABROS = 2;
+    public static final int COMMANDER = 0;
+    public static final int LEADER = 1;
+    public static final int NORMARL = 2;
+}
