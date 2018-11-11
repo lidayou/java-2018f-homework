d9e6b20e67cf90957279fa844eabf997c2af4138
DarkMagicXYZ <844081400@qq.com>
2018-11-11 21:18:15 +0800

    Homework1&2&3 version1.0

---
+++ 20180925/肖云泽-151180149/infos/Infos.java
@@ -0,0 +1,9 @@
+﻿package infos;
+
+public class Infos {
+	public static final int ARRAY_SCALE = 7;
+	public static final String[] CALABASHBROTHERQUEUE = {"0", "1", "2", "3", "4", "5", "6"};
+	public static final String[] ENEMYQUEUE = {"Scorpion", "Minion", "Minion", "Minion"};
+	public static final int BATTLEFIELD_SCALE = 15;
+}
+
