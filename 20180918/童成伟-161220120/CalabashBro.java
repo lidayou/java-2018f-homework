4f04195a1e0f627e86a19236d915098410aecea4
NJUTto <161220120@smail.nju.edu.cn>
2018-12-03 20:02:53 +0800

    Homework2

--- 20180918/童成伟-161220120/CalabashBro.java
+++ 20180918/童成伟-161220120/CalabashBro.java
@@ -2,8 +2,7 @@ package homework2;
 
 enum Name {老大,老二,老三,老四,老五,老六,老七}
 enum Color {红色,橙色,黄色,绿色,青色,蓝色,紫色}
-
- public class CalabashBro {
+public class CalabashBro {
 	public Color brocolor;
 	public Name broname;
 	//初始化
