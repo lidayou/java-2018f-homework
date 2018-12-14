55475a968d7a60e1d6abe22178744793fc678869
NJU-RY <18795705324@163.com>
2018-12-14 17:55:09 +0800

    Changes to be committed: 	new file:   Field.properties 	new file:   Image/1.png 	new file:   Image/10.png 	new file:   Image/11.png 	new file:   Image/2.png 	new file:   Image/3.png 	new file:   Image/4.png 	new file:   Image/5.png 	new file:   Image/6.png 	new file:   Image/7.png 	new file:   Image/8.png 	new file:   Image/9.png 	new file:   Image/background.png 	new file:   renyi2/Battlearray.java 	new file:   renyi2/Brothers.java 	new file:   renyi2/Creature.java 	new file:   renyi2/Field.java 	new file:   renyi2/FifthBattlearray.java 	new file:   renyi2/FirstBattlearray.java 	new file:   renyi2/ForthBattlearray.java 	new file:   renyi2/Grandpa.java 	new file:   renyi2/Main.java 	new file:   renyi2/NullCreature.java 	new file:   renyi2/Orchin.java 	new file:   renyi2/README.md 	new file:   renyi2/Scorpion.java 	new file:   renyi2/SecondBattlearray.java 	new file:   renyi2/SeventhBattlearray.java 	new file:   renyi2/SixthBattlearray.java 	new file:   renyi2/Snake_essence.java 	new file:   renyi2/ThirdBattlearray.java 	new file:   renyi2/UML.png 	new file:   renyi2/eighthBattlearray.java 	new file:   renyi2/gui.png

---
+++ 20180925/任意-161220101/hw3_3/renyi2/Scorpion.java
@@ -0,0 +1,12 @@
+package renyi2;
+
+public class Scorpion extends Creature {
+    Scorpion(int x,int y)
+    {
+        super("/Image/11.png");
+        this.Imageurl="/Image/11.png";
+        this.name="蝎子";
+        this.x=x;
+        this.y=y;
+    }
+}
